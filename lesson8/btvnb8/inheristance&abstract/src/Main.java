f6502df19f9429083bb201b7225be89d62bd7d30
Khoa-98 <anhkhoa98nd@gmail.com>
2022-01-06 11:28:45 +0700

    final btvnb8

--- lesson8/btvnb8/inheristance&abstract/src/Main.java
+++ lesson8/btvnb8/inheristance&abstract/src/Main.java
@@ -1,9 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Thông tin nhân viên: ");
+        Emlpoyee[] employee = new Emlpoyee[2];
+
         Waiter waiter = new Waiter(1, "Nam", 23, 5000000, 3000000);
         System.out.println(waiter);
 
         Kitchen kitchen = new Kitchen(2, "Thái Bảo", 40, 6000000, 5000000);
         System.out.println(kitchen);
+
+
     }
 }
